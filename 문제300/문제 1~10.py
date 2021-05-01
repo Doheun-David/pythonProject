0fe4cb0b032c7bd1aa665334e316582728c50863
Byungwoo Kim <rlaquddn05@naver.com>
2021-05-01 15:56:31 +0900

    ....

--- 문제300/문제 1~10.py
+++ 문제300/문제 1~10.py
@@ -1,3 +1,34 @@
 
 
 # 문제 1
+print("Hello World")
+
+# 문제 2
+print("Mary's cosmetics")
+
+# 문제 3
+print('신씨가 소리를 질렀다."도둑이야"')
+
+# 문제 4
+print("C:\Windows")
+
+# 문제 5
+#\n은 다음 줄로 옮겨주는 명령어
+#\t는 4칸을 띄어주는 명령어
+
+# 문제 6
+print("오늘은", "일요일")
+
+# 문제 7
+print("(sep = ';')")
+
+# 문제 8
+print("naver/kakao/sk/samsung")
+
+# 문제 9
+print("first")
+print("second")
+
+# 문제 10
+print(5/3)
+
