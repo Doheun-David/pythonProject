1a7d1159cc682450a79fa2b4f27dd62b1bfea61e
Byungwoo Kim <rlaquddn05@naver.com>
2021-05-01 17:29:21 +0900

    ....

---
+++ 문제300/문제 21~30.py
@@ -0,0 +1,30 @@
+# 문제 21 : 인덱싱[문자순서] : 0번시작
+letters = 'python'
+print( letters[0] , letters[2] )
+
+# 문제 22 : 슬라이싱[시작번호, 끝번호, 단위]
+    #시작번호부터 끝번호 전까지 문자 출력
+license_plate="24가 2210"
+print(license_plate[4:8])
+
+# 문제 23
+string = "홀짝홀짝홀짝"
+print(string[0:6:2]) # 0부터 6전까지 2씩 증가
+# 문제 24
+string = "PYTHON"
+print(string[ : : -1]) # 뒤에서부터 시작
+# 문제 25
+phone_number = "010-1111-2222"
+phone_number1 = phone_number.replace("-", " ")
+print(phone_number1)
+# 문제 26
+phone_number2 = phone_number1.replace(" ", "")
+print(phone_number2)
+
+# 문제 27
+
+# 문제 28
+
+# 문제 29
+
+# 문제 30
