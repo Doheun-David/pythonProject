d4f2af1247b7aa9eff7f8ecf3a9f284415ef0ee3
김도흔 <83488151+Doheun-David@users.noreply.github.com>
2021-07-03 15:56:21 +0900

    Add files via upload

---
+++ 리스트.py
@@ -0,0 +1,68 @@
+# 문제 51번
+movie_rank = ["닥터스트레인지", "스플릿", "럭키"]
+
+# 문제 52번
+movie_rank.append("배트맨")
+
+# 문제 53번
+movie_rank.append("슈퍼맨")
+
+# 문제 54번
+movie_rank.remove("럭키")
+
+# 문제 55번
+movie_rank.remove("스플릿")
+movie_rank.remove("배트맨")
+
+# 문제 56번
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+
+# 문제 57번
+nums = [1, 2, 3, 4, 5, 6, 7]
+print("max : ",max(nums))
+print("min : ",min(nums))
+
+# 문제 58번
+nums = [1, 2, 3, 4, 5]
+a = 0
+for i in range(len(nums)):
+    a += nums[i]
+print(a)
+
+# 문제 59번
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+# 문제 60번
+nums = [1, 2, 3, 4, 5]
+u = 0
+for i in range(len(nums)):
+    u += nums[i]
+print(u/len(nums))
+
+# 문제 61번
+price = ['20180728', 100, 130, 140, 150, 160, 170]
+
+# 문제 62번
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 문제 63번
+
+# 문제 64번
+nums = [1, 2, 3, 4, 5]
+
+# 문제 65번
+
+# 문제 66번
+
+# 문제 67번
+
+# 문제 68번
+
+# 문제 69번
+
+# 문제 70번
+
+
