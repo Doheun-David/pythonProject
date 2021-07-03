a61e389a91b526f61e98e35b058de50ebd20fda4
김도흔 <83488151+Doheun-David@users.noreply.github.com>
2021-07-03 17:41:25 +0900

    Add files via upload

--- 리스트.py
+++ 리스트.py
@@ -44,25 +44,62 @@ print(u/len(nums))
 
 # 문제 61번
 price = ['20180728', 100, 130, 140, 150, 160, 170]
+print(price[1 : ])
 
 # 문제 62번
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[0 : : 2])
 
 # 문제 63번
+print(nums[1 : :2])
 
 # 문제 64번
 nums = [1, 2, 3, 4, 5]
+print(nums[ : :-1])
 
 # 문제 65번
+interest = ['삼성전자', 'LG전자', 'Naver']
+print(interest[0], interest[2])
 
 # 문제 66번
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print(interest[0], interest[1], interest[2], interest[3], interest[4])
 
 # 문제 67번
+print(interest[0],"/",interest[1],"/",interest[2],"/",interest[3],"/",interest[4])
 
 # 문제 68번
+print(interest[0],"\n",interest[1],"\n",interest[2],"\n",interest[3],"\n",interest[4])
 
 # 문제 69번
+string = "삼성전자/LG전자/Naver"
+print(string.split("/"))
 
 # 문제 70번
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
 
+# 문제 71번
+my_variable = ( )
 
+# 문제 72번
+movie_rank = ("닥터 스트레인지", "스플릿", "럭키")
+
+# 문제 73번
+튜플 = (1)
+
+# 문제 74번
+t = (1, 2, 3, 4)
+
+# 문제 75번
+t = 1, 2, 3, 4
+
+# 문제 76번
+e = ('a', 'b', 'c')
+e[0] = 'A'
+# 문제 77번
+interest = ('삼성전자', 'LG전자', 'SK Hynix')
+
+# 문제 78번
+interest = ['삼성전자', 'LG전자', 'SK Hynix']
